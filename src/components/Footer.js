8390815a665ccb74b3513ea563388466338ad916
DK <darshankhot@programmer.net>
2020-10-31 13:50:35 +0530

    v2.0.0, updated footer links to HashRouter links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HashRouter as Router, Link } from 'react-router-dom'
 
 // Bootstrap
 import { Container, Row, Col } from 'react-bootstrap'
@@ -6,65 +7,71 @@ import { Container, Row, Col } from 'react-bootstrap'
 const Footer = () => {
     return(
         <footer className="bg-primary text-white pt-5 pb-3">
-            <Container>
-                <Row>
-                    <Col className="mb-4" sm={"12"} md={"3"} lg={"3"}>
-                        <h5>OPEN YOUR OWN KIDO CENTER</h5>
-                        <strong>
-                        Are you interested in operating your own preschool? Head to our franchise website to learn how you can launch your own Kïdo school.
-                        </strong>
-                    </Col>
-                    <Col className="mb-4 text-center" sm={"12"} md={"3"} lg={"3"}>
-                        <h5>
-                            <a href="#" className="text-white">CENTRES</a>
-                        </h5>
-                        <div>
-                            <a href="#" className="text-white">London</a>
-                        </div>
-                        <div>
-                            <a href="#" className="text-white">Windsor</a>
-                        </div>
-                        <div>
-                            <a href="#" className="text-white">Houston</a>
-                        </div>
-                        <div>
-                            <a href="#" className="text-white">Dubai</a>
-                        </div>
-                        <div>
-                            <a href="#" className="text-white">Hong Kong</a>
-                        </div>
-                    </Col>
-                    <Col className="mb-4 text-center" sm={"12"} md={"3"} lg={"3"}>
-                        <h5>
-                            <a href="#" className="text-white">ABOUT KIDO</a>
-                        </h5>
-                        <div>
-                            <a href="#" className="text-white">Kido Home</a>
-                        </div>
-                        <div>
-                            <a href="#" className="text-white">FAQ</a>
-                        </div>
-                        <div>
-                            <a href="#" className="text-white">Careers</a>
-                        </div>
-                        <div>
-                            <a href="#" className="text-white">News &amp; Events</a>
-                        </div>
-                        <div>
-                            <a href="#" className="text-white">Franchise</a>
-                        </div>
-                    </Col>
-                    <Col className="mb-4 text-center" sm={"12"} md={"3"} lg={"3"}>
-                        <h5>CONTACT</h5>
-                        <div><a href="mailto:hello@kido.school" className="text-white bg-secondary p-1 rounded">hello@kido.school</a></div>
-                        <address className="my-2">
-                            199 Bishopsgate<br />
-                            London, EC2M 3TY
-                        </address>
-                        <div><a href="#" className="text-white">YouTube</a></div>
-                    </Col>
-                </Row>
-            </Container>
+            <Router>
+                <Container>
+                    <Row>
+                        <Col className="mb-4" sm={"12"} md={"3"} lg={"3"}>
+                            <h5>OPEN YOUR OWN KIDO CENTER</h5>
+                            <strong>
+                            Are you interested in operating your own preschool? Head to our franchise website to learn how you can launch your own Kïdo school.
+                            </strong>
+                        </Col>
+                        <Col className="mb-4 text-center" sm={"12"} md={"3"} lg={"3"}>
+                            <h5>
+                                <Link to="/" className="text-white">CENTRES</Link>
+                            </h5>
+                            <div>
+                                <Link to="/" className="text-white">London</Link>
+                            </div>
+                            <div>
+                                <Link to="/" className="text-white">Windsor</Link>
+                            </div>
+                            <div>
+                                <Link to="/" className="text-white">Houston</Link>
+                            </div>
+                            <div>
+                                <Link to="/" className="text-white">Dubai</Link>
+                            </div>
+                            <div>
+                                <Link to="/" className="text-white">Hong Kong</Link>
+                            </div>
+                        </Col>
+                        <Col className="mb-4 text-center" sm={"12"} md={"3"} lg={"3"}>
+                            <h5>
+                                <Link to="/about" className="text-white">ABOUT KIDO</Link>
+                            </h5>
+                            <div>
+                                <Link to="/" className="text-white">Kido Home</Link>
+                            </div>
+                            <div>
+                                <Link to="/contact" className="text-white">FAQ</Link>
+                            </div>
+                            <div>
+                                <Link to="/contact" className="text-white">Careers</Link>
+                            </div>
+                            <div>
+                                <Link to="/about" className="text-white">News &amp; Events</Link>
+                            </div>
+                            <div>
+                                <Link to="/about" className="text-white">Franchise</Link>
+                            </div>
+                        </Col>
+                        <Col className="mb-4 text-center" sm={"12"} md={"3"} lg={"3"}>
+                            <h5>
+                                <Link to="/contact" className='text-white'>CONTACT</Link>
+                            </h5>
+                            <div><a href="mailto:hello@kido.school" className="text-white bg-secondary p-1 rounded">hello@kido.school</a></div>
+                            <address className="my-2">
+                                199 Bishopsgate<br />
+                                London, EC2M 3TY
+                            </address>
+                            <div>
+                                <Link to="/" className="text-white">YouTube</Link>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </Router>
             <hr/>
             <Container className="mb-0">
                 <Row>
